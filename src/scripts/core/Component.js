ff0f4f01077326e83ace6c4bc94ac09528d354ba
ludacirs <ludacirs@naver.com>
2021-12-05 23:27:24 +0900

    feat: Component Class 구현
    컴포넌트 단위로 관리하기 위한 상속 컴포넌트 클래스 생성

---
+++ src/scripts/core/Component.js
@@ -0,0 +1,46 @@
+export default class Component {
+  $target;
+  state;
+  props;
+
+  constructor($target, props = {}) {
+    this.$target = $target;
+    this.state = {};
+    this.props = props;
+    this.setup();
+    this.render();
+    this.setEvent();
+  }
+
+  template() {
+    return ``;
+  }
+  setup(className, tag = "div") {
+    this.$elem = document.createElement(tag);
+    this.$elem.classList.add(className);
+    this.$target.append(this.$elem);
+  }
+
+  setEvent() {}
+
+  addEvent(eventType, callback) {
+    this.$elem.addEventListener(eventType, callback);
+  }
+
+  render() {
+    this.$elem.innerHTML = this.template();
+    this.mounted();
+  }
+
+  setState(nextState) {
+    this.state = { ...nextState };
+    this.render();
+  }
+
+  $(selector) {
+    const elems = this.$elem.querySelectorAll(selector);
+    return elems.length === 1 ? elems[0] : elems;
+  }
+
+  mounted() {}
+}
