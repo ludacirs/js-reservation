059a22ace190ab29a792729764bfb92b69a598d8
ludacirs <ludacirs@naver.com>
2021-12-06 23:30:14 +0900

    feat: ReservationInfo 컴포넌트 구현
    대략적인 레이아웃 구현
    현재 클릭된 예약 정보를 받아서 렌더링하는 컴포넌트 구현

--- src/scripts/components/ReservationInfo/ReservationInfo.js
+++ src/scripts/components/ReservationInfo/ReservationInfo.js
@@ -1,37 +1,48 @@
 import "./ReservationInfo.scss";
 import Component from "../../core/Component";
+import { RESERVATION_STATUS } from "../../utils/Constant";
 
 class ReservationInfo extends Component {
+  state = {
+    currentInfo: null,
+  };
   setup() {
     super.setup("reservation-info-container", "div");
+    this.state.currentInfo = this.props.initReservation;
   }
 
-  render() {
+  template() {
+    const { status, timeReserved, timeRegistered, customer } =
+      this.state.currentInfo;
+    console.log(this.state.currentInfo);
+
     return `
+    <div class="header">예약 정보</div>
     <ul class="reservation-info-list">
       <li class="status">
-        <span>예약 상태</span> <span>value</span>
+        <span class="key">예약 상태</span> <span>${RESERVATION_STATUS[status]}</span>
       </li>
       <li class="reservation-time">
-      <span>예약 시간</span> <span>value</span>
+      <span class="key">예약 시간</span> <span>${timeReserved}</span>
 </li>
       <li class="reception-time">
-      <span>접수 시간</span> <span>value</span>
+      <span class="key">접수 시간</span> <span>${timeRegistered}</span>
 </li>
     </ul>
+    <div class="header">고객 정보</div>
     <ul class="customer-info-list">
         <li class="user-name">
-        <span>고객 성명</span> <span>value</span>
+        <span class="key">고객 성명</span> <span>${customer.name}</span>
 </li>
         <li class="user-rank">
-        <span>고객 등급</span> <span>value</span>
+        <span class="key">고객 등급</span> <span>${customer.level}</span>
 </li>
         <li class="user-memo">
-        <span>고객 메모</span> <span>value</span>
+        <div class="key">고객 메모</div> <div class="memo">${customer.memo}</div>
 </li>
     </ul>
     <ul class="request-info">
-        <span>요청사항</span> <span>value</span>
+        <span class="key">요청사항</span> <span>${customer.request}</span>
     </ul>
     `;
   }
